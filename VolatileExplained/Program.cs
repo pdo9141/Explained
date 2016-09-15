3f62643ed2e0f6af64bcfc6415e5f6cfdd5fad66
pdo9141 <pdo9141@gmail.com>
2016-09-14 21:37:11 -0700

    Add project files.

---
+++ VolatileExplained/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace VolatileExplained
+{
+    class Program
+    {
+        //private bool _loop = true;
+        private volatile bool _loop = true;
+
+        static void Main(string[] args)
+        {
+            // Building in Release and Debug mode makes a difference for this example, run in Release mode to show issue without volatile
+            // There's main memory that holds value for _loop as true
+            // There's also local virtual memory for each thread with value for _loop
+            // Changing test1._loop to false updates local virtual thread and main memory only even though you're changing a reference type value
+            // How do can you ensure Step 1-3 prints out properly and not just Step 1-2? You use the volatile keyword
+            // Using volatile will sync main and all local virtual memory
+            // Use volatile when doing multi-threaded applications and intend on updating data concurrently on multiple threads
+
+            Program test1 = new Program();
+            Thread obj = new Thread(SomeThread);
+            obj.Start(test1);
+
+            Thread.Sleep(20);
+
+            test1._loop = false;
+            Console.WriteLine("Step 2 :- The value is set to false");
+            Console.ReadLine();
+        }
+
+        private static void SomeThread(object o1)
+        {
+            Program o = (Program)o1;
+            Console.WriteLine("Step 1 :- Entered the loop");
+
+            while (o._loop)
+            {
+            }
+
+            Console.WriteLine("Step 3 :- Exited the loop");
+        }
+    }
+}
---
+++ YieldExplained/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldExplained
+{
+    class Program
+    {
+        private static List<int> _myList = new List<int>();
+
+        private static void FillValues()
+        {
+            _myList.Add(1);
+            _myList.Add(2);
+            _myList.Add(3);
+            _myList.Add(4);
+            _myList.Add(5);
+        }
+
+        static void Main(string[] args)
+        {
+            // Yield keyword helps you do custom stateful iteration over a collection
+            FillValues();
+            //ExecuteCustomIteration();
+            //ExecuteCustomIterationWithFilter();
+            ExecuteCustomIterationWithYield();
+            Console.WriteLine("");
+            ExecuteStatefulIteration();
+
+            Console.ReadLine();
+        }
+
+        private static void ExecuteCustomIteration()
+        {            
+            foreach (int i in _myList)
+                Console.WriteLine(i);
+        }
+
+        private static void ExecuteCustomIterationWithFilter()
+        {
+            // Now you only want to print values greater than 3
+            // Using Filter() forces you to use a new temporary list
+            foreach (int i in Filter())
+                Console.WriteLine(i);
+        }
+
+        private static void ExecuteCustomIterationWithYield()
+        {
+            // If you leverage Yield, you remove the need for a temporary list
+            // You basically yield control back to the caller then back into the filter with state of iteration persisted 
+            // Performance boost!
+            foreach (int i in FilterYield())
+                Console.WriteLine(i);
+        }
+
+        private static IEnumerable<int> Filter()
+        {
+            List<int> temp = new List<int>();
+            foreach (int i in _myList)
+                if (i > 3)
+                    temp.Add(i);
+        
+            return temp;
+        }
+
+        private static IEnumerable<int> FilterYield()
+        {
+            foreach (int i in _myList)
+                if (i > 3)
+                    yield return i;
+        }
+
+        private static void ExecuteStatefulIteration()
+        {
+            // If you leverage Yield you will perserve the value of runningTotal in RunningTotal()!
+            // Performance boost!
+            foreach (int i in RunningTotal())
+                Console.WriteLine(i);
+        }
+
+        private static IEnumerable<int> RunningTotal()
+        {
+            int runningTotal = 0;
+            foreach (int i in _myList)
+            {
+                runningTotal += i;
+                yield return (runningTotal);
+            }
+        }
+    }
+}
